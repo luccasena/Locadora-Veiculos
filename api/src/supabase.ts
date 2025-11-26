042e81e345bdd5707865e5aef99f139a7e483644
Maximus Feitoza Lira Cunha <151573378+MaxFeitoza@users.noreply.github.com>
2025-11-26 13:25:19 -0300

    Fix:Supabase url

--- api/src/supabase.ts
+++ api/src/supabase.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_URL_AUTH!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: {
