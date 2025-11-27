000a53b97863f1769aa8a98852038fff12cfeb0a
LEONARDO LUCAS DE BRITO SILVA <163042516+leonardolucasbs@users.noreply.github.com>
2025-11-27 14:01:56 -0300

    :recycle: refactor - Refactor loginPage and RegisterPage

--- front_end/services/userService.tsx
+++ front_end/services/userService.tsx
@@ -1,17 +1,17 @@
 import axios from "axios";
 import type { LoginRequest } from "../types/user/LoginRequest";
 import { ENV } from "../config/env";
-import { RegisterUser } from "@/types/user/RegisterUser";
+import { RegisterUserData } from "@/types/user/RegisterUser";
 import { UserUpdate } from "@/types/user/UserUpdate";
 
-const API_URL = ENV.NEXT_PUBLIC_API_URL
+const API_URL = ENV.NEXT_PUBLIC_API_URL;
 
 export const Login = async (login: LoginRequest) => {
   const response = await axios.post(`${API_URL}login/`, login);
   return response;
 };
 
-export const Register = async (usuario: RegisterUser) => {
+export const RegisterUser = async (usuario: RegisterUserData) => {
   const response = await axios.post(`${API_URL}clientes/`, usuario);
 
   return response;
@@ -20,4 +20,4 @@ export const Register = async (usuario: RegisterUser) => {
 export const updateUser = async (user: UserUpdate, id: number) => {
   const response = await axios.put(`${API_URL}clientes/${id}/`, user);
   return response;
-}
+};
--- front_end/types/user/UserUpdate.ts
+++ front_end/types/user/UserUpdate.ts
@@ -1,9 +1,8 @@
-
-export interface UserUpdate  {
-    cpf: string;
-    email: string;
-    name: string;
-    lastname?: string
-    phone: string;
-    password: string;
+export interface UserUpdate {
+  cpf: string;
+  email: string;
+  name: string;
+  lastname?: string;
+  phone: string;
+  password: string;
 }
