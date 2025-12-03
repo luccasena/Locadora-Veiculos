bfcefa6a272dcb87c0c517f2a70181279796546d
Lucca Sena <161384749+luccasena@users.noreply.github.com>
2025-12-03 17:26:17 -0300

    Change router paths to lowercase for consistency

--- front_end/app/home/page.tsx
+++ front_end/app/home/page.tsx
@@ -87,19 +87,19 @@ export default function HomePage() {
                                 <AdminCard 
                                 title="Contratos" 
                                 icon={<FileText className="text-white" size={48} />}
-                                onClick={() => router.push('Admin/manage-contracts')}
+                                onClick={() => router.push('admin/manage-contracts')}
                                 />
 
                                 <AdminCard 
                                 title="Carros" 
                                 icon={<Car className="text-white" size={48} />}
-                                onClick={() => router.push('Admin/manage-cars')}
+                                onClick={() => router.push('admin/manage-cars')}
                                 />
 
                                 <AdminCard 
                                 title="Clientes" 
                                 icon={<Users className="text-white" size={48} />}
-                                onClick={() => router.push('Admin/manage-clients')}
+                                onClick={() => router.push('admin/manage-clients')}
                                 />
 
                             </div>
