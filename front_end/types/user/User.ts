71d35f9d639d8af8800e34a790211d25de65d56f
Lucca Sena <luccasena22@gmail.com>
2025-12-03 01:38:14 -0300

    Update User.ts

---
+++ front_end/types/user/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    cpf: string;
+    email: string;
+    name: string;
+    lastname: string;
+    phone: string;
+    password: string;
+    createdAt: string;
+    updatedAt: string;
+}
