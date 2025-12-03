a9ebcce3efd3ab53bcc65607e5b42ff2f3579542
Maximus Feitoza Lira Cunha <151573378+MaxFeitoza@users.noreply.github.com>
2025-12-03 01:38:04 -0300

    Remove dist/ build files from version control

--- dist/db/prisma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../generated/prisma/index");
-const prisma = new index_1.PrismaClient();
-exports.default = prisma;
